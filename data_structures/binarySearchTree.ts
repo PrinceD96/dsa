21d3e6bc463032ae3bf735863b736ba5430752f7
Daniel Morales <daniel.a.moralesalt@gmail.com>
2022-07-29 16:05:17 -0400

    chore: add BFS and DFS

--- data_structures/binarySearchTree.ts
+++ data_structures/binarySearchTree.ts
@@ -64,6 +64,41 @@ class BinarySearchTree {
 
 		return currentNode;
 	}
+
+	breadthFirstSearch(bst: BinarySearchTree): number[] | null {
+		if (!bst.root) return null;
+		const nodesQueue = [];
+		const visited = [];
+
+		nodesQueue.push(bst.root);
+		while (nodesQueue.length) {
+			//@ts-ignore
+			const currentNode: Node = nodesQueue.shift();
+			visited.push(currentNode!.value);
+			if (currentNode!.left) {
+				nodesQueue.push(currentNode!.left);
+			}
+			if (currentNode!.right) {
+				nodesQueue.push(currentNode!.right);
+			}
+		}
+		return visited;
+	}
+
+	recursiveDFS(bst: BinarySearchTree): number[] | null {
+		if (!bst.root) return null;
+		const visited: number[] = [];
+		const currentNode = bst.root;
+
+		const preOrderDFS = (node: Node): void => {
+			visited.push(node.value);
+			if (node.left) preOrderDFS(node.left);
+			if (node.right) preOrderDFS(node.right);
+		};
+		preOrderDFS(currentNode);
+
+		return visited;
+	}
 }
 
 const binarySearchTree = new BinarySearchTree();
@@ -77,3 +112,5 @@ binarySearchTree.insert(10);
 binarySearchTree.insert(6);
 
 binarySearchTree.find(8);
+binarySearchTree.breadthFirstSearch(binarySearchTree); // [5,3,8,2,4,7,10, 6]
+binarySearchTree.recursiveDFS(binarySearchTree); // [5,3,2,4,8,7,6,10]
